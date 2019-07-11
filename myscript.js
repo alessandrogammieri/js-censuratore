09e66a9650d091e8994fd1e6011365dd042455e1
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-11 19:25:18 +0200

    end censuratore

--- myscript.js
+++ myscript.js
@@ -11,31 +11,36 @@ var testo, parolacce, counter;
 // Chiedo all'utente di inserire un testo
 testo = prompt("Inserisci qui un testo").split(/\W+/g);
 /* Testo da inserire:
-Lorem? ipsum dolor sit amet, consectetur adipisicing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-veniam, quis troia exercitation ullamco laboris puttana ut aliquip ex ea commodo
-consequat. Duis aute irure vaffanculo in reprehenderit in voluptate velit esse cillum
-dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-sunt in culpa qui officia deserunt mollit anim id cazzo laborum. */
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum. */
 console.log(testo);
 
 // Chiedo all'utente le parole da censurare
 parolacce = prompt("Inserisci qui le parole che devono essere censurate").split(/\W+/g);
-/* Parole da censurare: cazzo vaffanculo puttana troia */
+/* Parole da censurare:
+Excepteur sint occaecat cupidatat non proident */
 console.log(parolacce);
 
-// Restituire il testo con xxx al posto delle parole censurate
-counter = 0;
-for (var i = 0; i < testo.length; i++) {
-  if (testo.includes(parolacce[i])) {
-    parolacce[i] = "xxx";
-    counter++;
-
+// Funzione che restituire il testo con xxx al posto delle parole censurate
+function inquisitore(testo1, testo2) {
+  counter = 0;
+  for (var i = 0; i < testo1.length; i++) {
+    if (testo2.includes(testo1[i]) == true) {
+      testo1[i] = "xxx";
+      counter++;
+    }
   }
 }
-console.log(parolacce);
-console.log(counter);
 
+// Applichiamo la funzione ai 2 array
+inquisitore(testo, parolacce)
+
+// Restituiamo il testo con xxx al posto delle parole censurate.
+document.getElementById('tuo_id').innerHTML = testo.join(" ");
 
 // “Badword index” con il numero di parole censurate su il numero di parole totali.
 document.getElementById('mio_id').innerHTML = counter + " parolacce " + "su " + testo.length + " parole totali";
